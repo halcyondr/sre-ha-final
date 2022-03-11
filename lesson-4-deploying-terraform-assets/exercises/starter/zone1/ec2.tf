496212d1690b86ca94e8eeadd85aeea19da29907
tommaso808 <65737680+tommaso808@users.noreply.github.com>
2022-03-11 09:55:14 -0600

    lessn4
    try 1

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -1,10 +1,39 @@
   module "project_ec2" {
-   source             = 
-   instance_count     = 
+   source             = "./modules/ec2"
+   instance_count     = 2
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
    aws_ami            = "ami-0b9064170e32bde34"
    private_subnet_ids = module.vpc.private_subnet_ids
-   public_subnet_ids  = 
-   vpc_id             = 
+   public_subnet_ids  = module.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_id
+ resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  vpc_id      = var.vpc_id
+
+  ingress {    
+    description = "web port"
+    from_port   = 80    
+    to_port     = 80
+    protocol    = "tcp"    
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh port"
+    from_port   = 22    
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
  }
