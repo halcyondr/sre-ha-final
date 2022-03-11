35e3917c9c30c41c97919f28cf1800075eea6e56
tommaso808 <65737680+tommaso808@users.noreply.github.com>
2022-03-11 10:03:28 -0600

    lesson4
    try2

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -7,33 +7,4 @@
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
- resource "aws_security_group" "ec2_sg" {
-  name        = "ec2_sg"
-  vpc_id      = var.vpc_id
-
-  ingress {    
-    description = "web port"
-    from_port   = 80    
-    to_port     = 80
-    protocol    = "tcp"    
-    cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "ssh port"
-    from_port   = 22    
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ec2_sg"
-  }
- }
