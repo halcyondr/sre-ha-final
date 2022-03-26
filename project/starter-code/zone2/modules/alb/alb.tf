0de8132e5f6d0b92338b7977fd25b42ee6a4de39
tommaso808 <65737680+tommaso808@users.noreply.github.com>
2022-03-25 22:44:57 -0500

    Update alb.tf
    76

--- project/starter-code/zone2/modules/alb/alb.tf
+++ project/starter-code/zone2/modules/alb/alb.tf
@@ -2,8 +2,8 @@ resource "aws_lb_target_group" "udacity" {
   name     = "udacity-lb-tg"
   port     = 80
   protocol = "HTTP"
-#  vpc_id   = var.vpc_id
-  vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id 
+  vpc_id   = var.vpc_id
+#  vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id 
 }
 
 resource "aws_lb_target_group_attachment" "udacity" {
